8b3933ad1f47486dc5ff130797782f8c6e298333
VermaRahul <vermasonr@gmail.com>
2014-08-03 18:20:03 -0400

    Adding Settings Manager

--- Tasker.PCL/ViewModel/ViewModelLocator.cs
+++ Tasker.PCL/ViewModel/ViewModelLocator.cs
@@ -3,32 +3,16 @@ using Microsoft.Practices.ServiceLocation;
 
 namespace Tasker.PCL.ViewModel
 {
-    /// <summary>
-    /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
-    /// </summary>
     public class ViewModelLocator
     {
-        /// <summary>
-        /// Initializes a new instance of the ViewModelLocator class.
-        /// </summary>
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
-            //if (ViewModelBase.IsInDesignModeStatic)
-            //{
-            //    SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
-            //}
-            //else
-            //{
-            //    SimpleIoc.Default.Register<IDataService, DataService>();
-            //}
-
             SimpleIoc.Default.Register<MainViewModel>();
         }
 
-        public MainViewModel Main
+        public MainViewModel MainViewModel
         {
             get
             {
@@ -36,9 +20,6 @@ namespace Tasker.PCL.ViewModel
             }
         }
         
-        /// <summary>
-        /// Cleans up all the resources.
-        /// </summary>
         public static void Cleanup()
         {
             // TODO Clear the ViewModels
---
+++ Tasker/Utils/SettingsManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Tasker.PCL.Model;
+
+namespace Tasker.Utils
+{
+    public class SettingsManager
+    {
+        private Dictionary<string, string> _settingsDictionary;
+
+        private const string SettingsKey = "Settings";
+
+        private const string AccessToken = "AccessToken";
+
+        private IsolatedStorageSettings _settings;
+
+        public SettingsManager()
+        {
+            _settings = IsolatedStorageSettings.ApplicationSettings;
+            object creds = null;
+
+            if (_settings.TryGetValue(SettingsKey, out creds))
+            {
+                _settingsDictionary = creds as Dictionary<string, string>;
+            }
+            else
+            {
+                _settingsDictionary = new Dictionary<string, string>();
+                _settings[SettingsKey] = _settingsDictionary;
+            }
+        }
+
+        public AppSettings RetrieveSettings()
+        {
+            try
+            {
+                if (_settingsDictionary == null || !_settingsDictionary.Any())
+                    return null;
+
+                var appSettings = new AppSettings();
+
+                if(_settingsDictionary.ContainsKey(AccessToken))
+                    appSettings.AccessToken = GetUnprotectedDataString(_settingsDictionary[AccessToken]);
+
+                return appSettings.IsValid() ? appSettings : null;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Error loading settings {0}", e);
+            }
+
+            return null;
+        }
+
+        public bool RemoveSettings()
+        {
+            var res = _settings.Remove(SettingsKey);
+            _settings.Save();
+            return res;
+        }
+
+        private bool SaveSetting(string value, string name, bool isProtected)
+        {
+            try
+            {
+                _settingsDictionary[name] = isProtected ? GetProtectedDataString(value) : value;
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Failed to save {0} to vault: {1}", name, e);
+            }
+
+            return false;
+        }
+
+        private string GetProtectedDataString(string unprotectedData)
+        {
+            var unprotectedBytes = Encoding.UTF8.GetBytes(unprotectedData);
+            var protectedBytes = ProtectedData.Protect(unprotectedBytes, null);
+
+            return Convert.ToBase64String(protectedBytes);
+        }
+
+        private string GetUnprotectedDataString(string protectedData)
+        {
+            var protectedBytes = Convert.FromBase64String(protectedData);
+            var unprotectedBytes = ProtectedData.Unprotect(protectedBytes, null);
+
+            return Encoding.UTF8.GetString(unprotectedBytes, 0, unprotectedBytes.Length);
+        }
+    }
+}
