a0424bdd2c0d2b8abb83eb9482cf9bb8a839ab12
VermaRahul <vermasonr@gmail.com>
2014-08-03 11:33:49 -0400

    Initial Commit

---
+++ Tasker.PCL/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Tasker.PCL.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+           
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
