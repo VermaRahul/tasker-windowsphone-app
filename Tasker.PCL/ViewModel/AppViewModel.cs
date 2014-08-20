0e01daafaa3ea60787017725cb28c183669d9c54
VermaRahul <vermasonr@gmail.com>
2014-08-19 21:06:15 -0400

    Modified baseVM

--- Tasker.PCL/ViewModel/AppViewModel.cs
+++ Tasker.PCL/ViewModel/AppViewModel.cs
@@ -18,5 +18,63 @@ namespace Tasker.PCL.ViewModel
             Context = context;
             NavigationService = navigationService;
         }
+
+        public virtual void CleanUp()
+        {
+        }
+
+        public virtual void SetData(object context)
+        {
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// The <see cref="IsLoading" /> property's name.
+        /// </summary>
+        public const string IsLoadingPropertyName = "IsLoading";
+
+        private bool _isLoading = false;
+
+        /// <summary>
+        /// Sets and gets the IsLoading property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set { Set(IsLoadingPropertyName, ref _isLoading, value); }
+        }
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler<AppError> OnError;
+
+        public void InvokeError(AppError error)
+        {
+            if(OnError != null)
+                OnError.Invoke(this, error);
+        }
+
+        public void InvokeError(string message, string title)
+        {
+            InvokeError(new AppError(message, title));
+        }
+
+        #endregion
+    }
+
+    public class AppError : EventArgs
+    {
+        public readonly string Message;
+        public readonly string Title;
+
+        public AppError(string message, string title)
+        {
+            Message = message;
+            Title = title;
+        }
     }
 }
