075f9a9eade834f8adceaffdffc382ee063d0d2c
VermaRahul <vermasonr@gmail.com>
2014-08-23 19:39:12 -0400

    Added Visibility converter and another converter that switches visibility if the item type is "Task".

---
+++ Tasker/Converters/IsTaskComponentConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using Tasker.PCL.Enumerations;
+
+namespace Tasker.Converters
+{
+    public class IsTaskComponentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is ObjectType && (ObjectType)value == ObjectType.Task) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
