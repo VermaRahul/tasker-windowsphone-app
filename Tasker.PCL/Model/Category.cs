7b8f2af158f0bd7b38bba3a5f7c7bc2aa6fb66c7
VermaRahul <vermasonr@gmail.com>
2014-08-24 00:35:47 -0400

    Added Model for categories.
    Including json.net in wp8 solution

---
+++ Tasker.PCL/Model/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Tasker.PCL.Utils;
+
+namespace Tasker.PCL.Model
+{
+    public class Category
+    {
+        [DataMember, JsonProperty(PropertyName = JsonConstants.Name)]
+        public string Name { get; set; }
+    }
+}
---
+++ Tasker.PCL/Model/JsonData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Tasker.PCL.Utils;
+
+namespace Tasker.PCL.Model
+{
+    public class JsonData
+    {
+        [DataMember, JsonProperty(PropertyName = JsonConstants.Categories)]
+        public List<Category> Categories;
+
+        public JsonData DeepCopy()
+        {
+            var ser = JsonConvert.SerializeObject(this);
+            var des = JsonConvert.DeserializeObject<JsonData>(ser);
+
+            return des;
+        }
+    }
+}
