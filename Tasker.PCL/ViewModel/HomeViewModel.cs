b9e8f051185a799af2d2709e4289d9ec01b30045
VermaRahul <vermasonr@gmail.com>
2014-08-24 01:13:07 -0400

    include

--- Tasker.PCL/ViewModel/HomeViewModel.cs
+++ Tasker.PCL/ViewModel/HomeViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
 using Tasker.PCL.Enumerations;
 using Tasker.PCL.Model;
 using Tasker.PCL.Utils;
