570d8b3bf79ab077f5828067cc9352077ec20436
VermaRahul <vermasonr@gmail.com>
2014-08-24 22:04:37 -0400

    Changed Style of home page.

---
+++ Tasker/Converters/CategoryToTextConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Tasker.PCL.Model;
+
+namespace Tasker.Converters
+{
+    public class CategoryToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Category)value == null)
+            {
+                return string.Empty;
+            }
+            return ((Category) value).Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tasker/Converters/PriorityToBrushConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using Tasker.PCL.Enumerations;
+
+namespace Tasker.Converters
+{
+    public class PriorityToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Priority)value == Priority.Normal)
+            {
+                return (SolidColorBrush)(Application.Current.Resources["TasksLowPriorityBrush"]);
+            }
+            else
+            {
+                return (SolidColorBrush)(Application.Current.Resources["TasksHighPriorityBrush"]);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
