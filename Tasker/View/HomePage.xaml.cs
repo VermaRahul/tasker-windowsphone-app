afae510dda139731c1b6b7991bc10df4d8a86899
VermaRahul <vermasonr@gmail.com>
2014-08-26 12:18:49 -0400

    Added application bar in codebehind

--- Tasker/View/HomePage.xaml.cs
+++ Tasker/View/HomePage.xaml.cs
@@ -16,9 +16,54 @@ namespace Tasker.View
 {
     public partial class HomePage : AppPage
     {
+        private ApplicationBarIconButton _addEventButton;
+        private ApplicationBarIconButton _addCategoryButton;
         public HomePage()
         {
             InitializeComponent();
+            CreateApplicationBar();
+        }
+
+        private void CreateApplicationBar()
+        {
+            ApplicationBar = new ApplicationBar
+            {
+                BackgroundColor = (Color) (Application.Current.Resources["PageHeaderBackgroundColor"]),
+                ForegroundColor = Colors.White
+            };
+
+            _addEventButton = new ApplicationBarIconButton
+            {
+                IconUri = new Uri("/Assets/AppBar/ApplicationBar.Add.png", UriKind.Relative),
+                Text = "Event"
+            };
+            _addEventButton.Click += AddEventButtonClicked;
+            ApplicationBar.Buttons.Add(_addEventButton);
+            _addCategoryButton = new ApplicationBarIconButton
+            {
+                IconUri = new Uri("/Assets/AppBar/ApplicationBar.Add.png", UriKind.Relative),
+                Text = "Category"
+            };
+            _addCategoryButton.Click += AddCategoryButtonClicked;
+
+        }
+
+        private void AddCategoryButtonClicked(object sender, EventArgs e)
+        {
+            var _vm = DataContext as HomeViewModel;
+            if (_vm == null)
+                return;
+
+            _vm.AddNewButtonPressedCommand.Execute(ObjectType.Category);
+        }
+
+        private void AddEventButtonClicked(object sender, EventArgs e)
+        {
+            var _vm = DataContext as HomeViewModel;
+            if(_vm == null)
+                return;
+
+            _vm.AddNewButtonPressedCommand.Execute(ObjectType.Event);
         }
 
         private void CategoriesListSelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -46,6 +91,23 @@ namespace Tasker.View
 
             _vm.NavigationService.NavigateTo<EventsViewModel>(navData);
 
+        }
+
+        private void OnPivotSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ApplicationBar.Buttons.Clear();
+            var pivotItem = ContentPivot.SelectedItem as PivotItem;
+            if (pivotItem == null)
+                return;
+            if(pivotItem.Header.Equals("Events"))
+            {
+                ApplicationBar.Buttons.Add(_addEventButton);
+            }
+            else if (pivotItem.Header.Equals("Categories"))
+            {
+                ApplicationBar.Buttons.Add(_addCategoryButton);
+                
+            }
         }
     }
 }
